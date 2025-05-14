67082a8baed6d16b1a902ff8ac4a388dfab8c60d
Mari Hayashi <mhaya010@ucr.edu>
2025-05-13 20:39:55 -0700

    Created React project

---
+++ Source/client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './kishi.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Mizuki!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
