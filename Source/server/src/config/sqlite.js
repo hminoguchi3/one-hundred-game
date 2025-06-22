a0473b69a7221dbee0c510bb12be96c6a63046af
hminoguchi3 <hajimeminoguchi@gmail.com>
2025-06-21 22:25:39 -0400

    fixed sqlite error?

--- Source/server/src/config/sqlite.js
+++ Source/server/src/config/sqlite.js
@@ -1,8 +1,16 @@
 // Load path from environment variables
 // (The first require of dotenv should be in index.js; we assume it's already run)
 
+// Added by Hajime
+const fs   = require('fs');
+const path = require('path');
+
 // Import the better-sqlite3 driver
 const Database = require('better-sqlite3');
+
+// Added by Hajime
+const dbFile = process.env.DB_PATH || path.join(__dirname, '..', '..', 'data', 'rooms.db');
+fs.mkdirSync(path.dirname(dbFile), { recursive: true });
 
 // Open or create the DB file specified in .env
 const db = new Database(process.env.DB_PATH);
